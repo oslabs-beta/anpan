a65553a80c80f83e2357d64e606eeed226e4d6f7
Kevin Murphy <murph212@yahoo.com>
2024-02-13 17:30:49 -0500

    isRequired functionality is installed and working. But need to discuss how to implement Throw Error.

--- repository.ts
+++ repository.ts
@@ -1,6 +1,6 @@
-const ULID = require('ulid');
-import { Schema } from './schema.ts';
-import type { Client } from './types.ts';
+const ULID = require("ulid");
+import { Schema } from "./schema.ts";
+import type { Client } from "./types.ts";
 
 export class Repository {
   schema: Schema;
@@ -11,56 +11,68 @@ export class Repository {
     this.client = client;
   }
   async save(entity: object) {
+    //intialize object type that takes a string "key" as the key name and a string ('not found') as the value.  This object will be populated with the required fields as set in the optional "required: true" parameter during schema instantiation. The following lines iterate thru the schema of the entity fields to identify the required keys
+
+    const requiredKeys: { [index: string]: string } = {};
+    for (let k = 0; k < Object.entries(this.schema.fields).length; k++) {
+      let keyName = Object.keys(this.schema.fields)[k];
+      console.log("**key: ", keyName);
+      if (this.schema.fields[keyName].required) {
+        if (this.schema.fields[keyName].required === true) {
+          requiredKeys[keyName] = "notFound";
+        }
+      }
+    }
+
     // loop through entity
     // check if entity has key which matches schema key
     for (let [key, value] of Object.entries(entity)) {
-      console.log(this.schema);
       if (!this.schema.fields.hasOwnProperty(key))
         throw new Error(`schema does not have field ${key}`);
+      //check to see if this is a required key; if it is, annotate "Found" on the requiredKeys object
+      if (requiredKeys.hasOwnProperty(key)) {
+        requiredKeys[key] = "Found";
+      }
 
       // check if the type of the property matches the "type" proprty of the corresponding schema field
       switch (this.schema.fields[key].type) {
-        case 'string':
-          if (typeof value !== 'string')
+        case "string":
+          if (typeof value !== "string")
             throw new Error(`${key} must be of type string`);
           break;
-        case 'boolean':
-          if (typeof value !== 'boolean')
+        case "boolean":
+          if (typeof value !== "boolean")
             throw new Error(`${key} must be of type boolean`);
           break;
-        case 'number':
-          if (typeof value !== 'number')
+        case "number":
+          if (typeof value !== "number")
             throw new Error(`${key} must be of type number`);
           break;
-        case 'date':
+        case "date":
           if (!(value instanceof Date))
             throw new Error(`${key} must be of type Date`);
           break;
-        case 'point':
+        case "point":
           if (
             value !== null &&
-            typeof value === 'object' &&
+            typeof value === "object" &&
             !Array.isArray(value) &&
             !(value instanceof Date) &&
             Object.keys(value).length === 2 &&
-            typeof value.latitude === 'number' &&
-            typeof value.longitude === 'number'
+            typeof value.latitude === "number" &&
+            typeof value.longitude === "number"
           )
             throw new Error(`${key} must be of type point`);
           break;
       }
     }
-
-    // const EntityKeyName = Symbol(ULID.ulid());
-    // entity['EntityKeyName'] = EntityKeyName;
-
-    // need to generate the EntityKeyName
-    // const keyName = entity[EntityKeyName]!;
-
-    // await this.client.json.set(EntityKeyName.toString(), '$', entity);
-    await this.client.json.set(ULID.ulid(), '$', entity);
-
-    // await this.client.set('test1', 'may');
-    return;
+    //check to see if the requiredKeys object has any keys with value notFound. If so, throw error.
+    console.log("**requiredKeys at end of looping is: ", requiredKeys);
+    // if (Object.values(requiredKeys).includes("notFound"))
+    //   throw new Error(
+    //     `must provide all required fields as specified in schema definition`
+    //   );
   }
+
+  return;
 }
--- types.ts
+++ types.ts
@@ -1,11 +1,18 @@
+// export interface Fields {
+//   [index: string]: {
+//     type: string}
+//     }
+
 export interface Fields {
   [index: string]: {
-    type: string;
+    type: string | boolean,
+    required?: boolean;
   };
 }
+
 export interface Client {
   json: {
     set: Function;
   };
 }
-export type StructureOption = 'HASH' | 'JSON';
+export type StructureOption = "HASH" | "JSON";
