f7d9ae2cfba9e64ee6298784c34e70707667324c
rehcliw <rehcliw@gmail.com>
2024-02-17 11:03:12 -0500

    started repo

--- __tests__/repository.test.ts
+++ __tests__/repository.test.ts
@@ -1,4 +1,32 @@
-import { describe, expect, test, beforeEach } from 'bun:test';
-import { Repository } from '..';
+import { describe, expect, test, beforeEach, mock } from 'bun:test';
+import { Repository, Schema } from '..';
+import type { Client } from '../types.ts'
 
-describe('Repository', () => {});
+describe('Repository', () => {
+  let schema: Schema;
+  let repo: Repository;
+  let client: Client;
+  
+  describe('fetch', ()=> {
+    // create a false client
+    beforeEach(() => {
+      schema = new Schema('TestEntity', {
+        aBoolean: { type: 'boolean' },
+        aNumber: { type: 'number' },
+        aString: { type: 'string' },
+      });
+      client = 
+        {
+          json: {
+            set: mock(() => {}),
+            get: mock(() => {}),
+            del: mock(() => {}),
+          },
+          expire: mock(() => {}),
+          keys: mock(() => {}),
+        }
+      
+      repo = new Repository(schema, client)
+    })
+  })
+});
