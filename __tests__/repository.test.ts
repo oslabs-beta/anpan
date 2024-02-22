8903bebad2bffa5beb83162905721b68647b5a70
Kevin Murphy <murph212@yahoo.com>
2024-02-22 10:36:26 -0500

    added testing for 'save' and partial functionality for 'remove'

--- __tests__/repository.test.ts
+++ __tests__/repository.test.ts
@@ -1,25 +1,52 @@
-import { describe, expect, test, beforeEach, mock } from 'bun:test';
-import { Repository, Schema } from '..';
-import type { Client } from '../types.ts';
+import { describe, expect, test, beforeEach, mock } from "bun:test";
+import { Repository, Schema } from "..";
+import type { Client } from "../types.ts";
 
-describe('Repository', () => {
+describe("Repository", () => {
   let schema: Schema;
   let repo: Repository;
   let client: Client;
 
-  describe('fetch', () => {
+  // describe("fetch", () => {
+  //   beforeEach(() => {
+  //     schema = new Schema("TestEntity", {
+  //       aBoolean: { type: "boolean" },
+  //       aNumber: { type: "number" },
+  //       aString: { type: "string" },
+  //     });
+  //     // create a false client
+  //     client = {
+  //       json: {
+  //         set: mock(() => {}),
+  //         get: mock(() => {}),
+  //         del: mock(() => {}),
+  //       },
+  //       expire: mock(() => {}),
+  //       keys: mock(() => {}),
+  //     };
+
+  //     repo = new Repository(schema, client);
+  //   });
+
+  //   test("client get is called", () => {
+  //     repo.fetch("fakeULID");
+  //     expect(client.json.get).toHaveBeenCalled();
+  //   });
+  // });
+
+  describe("remove", () => {
     beforeEach(() => {
-      schema = new Schema('TestEntity', {
-        aBoolean: { type: 'boolean' },
-        aNumber: { type: 'number' },
-        aString: { type: 'string' },
+      schema = new Schema("TestEntity", {
+        aBoolean: { type: "boolean" },
+        aNumber: { type: "number" },
+        aString: { type: "string" },
       });
       // create a false client
       client = {
         json: {
+          del: mock(() => {}),
           set: mock(() => {}),
           get: mock(() => {}),
-          del: mock(() => {}),
         },
         expire: mock(() => {}),
         keys: mock(() => {}),
@@ -28,9 +55,55 @@ describe('Repository', () => {
       repo = new Repository(schema, client);
     });
 
-    test('client get is called', () => {
-      repo.fetch('fakeULID');
-      expect(client.json.get).toHaveBeenCalled();
+    // create a false/test entity
+    const myEntity = {
+      aBoolean: true,
+      aNumber: 4,
+      aString: "this is a test entity",
+    };
+
+    // test("client get is called", () => {
+    //   repo.remove("fakeULID");
+    //   expect(client.json.get).toHaveBeenCalled();
+    // });
+
+    test("client del is called", () => {
+      repo.remove("fakeULID");
+      expect(client.json.del).toHaveBeenCalled();
     });
   });
+
+//   describe("save", () => {
+//     beforeEach(() => {
+//       schema = new Schema("TestEntity", {
+//         aBoolean: { type: "boolean" },
+//         aNumber: { type: "number" },
+//         aString: { type: "string" },
+//       });
+//       // create a false client
+//       client = {
+//         json: {
+//           set: mock(() => {}),
+//           get: mock(() => {}),
+//           del: mock(() => {}),
+//         },
+//         expire: mock(() => {}),
+//         keys: mock(() => {}),
+//       };
+
+//       repo = new Repository(schema, client);
+//       console.log("repo: ", repo);
+//     });
+//     //create a false/test entity
+//     const myEntity = {
+//       aBoolean: true,
+//       aNumber: 4,
+//       aString: "this is a test entity",
+//     };
+
+//     test("client set is called", () => {
+//       repo.save(myEntity);
+//       expect(client.json.set).toHaveBeenCalled();
+//     });
+//   });
 });
--- __tests__/schema.test.ts
+++ __tests__/schema.test.ts
@@ -1,29 +1,29 @@
-import { describe, expect, test, beforeEach } from 'bun:test';
-import { Schema } from '..';
+import { describe, expect, test, beforeEach } from "bun:test";
+import { Schema } from "..";
 
-describe('Schema', () => {
+describe("Schema", () => {
   let schema: Schema;
 
-  describe('that defines fields', () => {
+  describe("that defines fields", () => {
     beforeEach(() => {
-      schema = new Schema('TestEntity', {
-        aBoolean: { type: 'boolean' },
-        aNumber: { type: 'number' },
-        aString: { type: 'string' },
+      schema = new Schema("TestEntity", {
+        aBoolean: { type: "boolean" },
+        aNumber: { type: "number" },
+        aString: { type: "string" },
       });
     });
 
-    test('returns a field by name', () => {
+    test("returns a field by name", () => {
       const field = schema.getAllFields().aBoolean;
       expect(field).toBeDefined();
       // expect(field.aBoolean).toBe('aBoolean')
-      expect(field.type).toBe('boolean');
+      expect(field.type).toBe("boolean");
     });
 
-    test('returns schema name', () => {
+    test("returns schema name", () => {
       const schemaName = schema.getSchemaName();
       expect(schemaName).toBeDefined();
-      expect(schemaName).toBe('TestEntity');
+      expect(schemaName).toBe("TestEntity");
     });
   });
 });
